bf52205bf6ab2041e28c43495018e3951c5eef09
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2024-08-13 14:29:20 -0500

    Update every.go - simplify comparison

--- every.go
+++ every.go
@@ -4,7 +4,7 @@ package jslice
 // meeting provided condition.
 func Every[T any](s []T, fn func(int, T) bool) bool {
 	for i, e := range s {
-		if r := fn(i, e); r == false {
+		if !fn(i, e) {
 			return false
 		}
 	}
