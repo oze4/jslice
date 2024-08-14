6b7e20f0d6c2e7b8cb701de83524143cf567afa6
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2024-08-14 14:20:37 -0500

    go fmt

--- shift.go
+++ shift.go
@@ -3,7 +3,7 @@ package jslice
 // Shift removes the first element from a slice and returns that element.
 // Shift changes the lenght of an array.
 func Shift[T any](s *[]T) T {
-    first := (*s)[0]
-    *s = (*s)[1:]
-    return first
+	first := (*s)[0]
+	*s = (*s)[1:]
+	return first
 }
