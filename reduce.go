ed5b7e5af818bea3a50fcef859f2159e7962b741
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2024-10-10 10:46:03 -0500

    chore: rename params

--- reduce.go
+++ reduce.go
@@ -1,9 +1,9 @@
 package jslice
 
-// Reduce reduce a slice
-func Reduce[I any, O any](source []I, reducer func(accumulator O, currentElement I, currentIndex int, originalSlice []I) O, initialValue O) O {
-	prevElement := initialValue
-	out := initialValue
+// Reduce reduces a slice
+func Reduce[InputType any, OutputType any](source []InputType, reducer func(acc OutputType, currEl InputType, currIdx int, ogSlice []InputType) OutputType, initVal OutputType) OutputType {
+	prevElement := initVal
+	out := initVal
 
 	for i := range source {
 		out = reducer(prevElement, source[i], i, source)
